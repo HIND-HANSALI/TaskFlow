04bb345694ceb3d33703ea9812499a9bfc158fb2
HIND-HANSALI <hindhsl111@gmail.com>
2023-12-25 12:01:22 +0100

    add tag entity

--- src/main/java/com/taskflow/models/Tag.java
+++ src/main/java/com/taskflow/models/Tag.java
@@ -1,2 +1,27 @@
-package com.taskflow.models;public class Tag {
+package com.taskflow.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String title;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Task> tasks;
+
 }
